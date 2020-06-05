afa452670c474bd0252b990aaa59eddfdc85997d
yuzu0o0 <55910981+yuzu0o0@users.noreply.github.com>
2020-06-05 23:05:01 +0545

    Order frag completed.

--- app/src/main/java/com/example/coursework/OrderNav.java
+++ app/src/main/java/com/example/coursework/OrderNav.java
@@ -8,11 +8,60 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class OrderNav extends Fragment {
+    RecyclerView recyclerView;
+    FoodAdapter adapter;
+
+    List<FoodList> foodlist;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.order_frag, container, false);
+
+        View view = inflater.inflate(R.layout.order_frag, container, false);
+
+        foodlist = new ArrayList<>();
+
+        recyclerView = view.findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        foodlist.add(
+                new FoodList(
+                        "300",
+                        R.drawable.burger,
+                        "Burger"
+
+                ));
+
+        foodlist.add(
+                new FoodList(
+                        "300",
+                        R.drawable.momo,
+                        "Burger"
+
+                ));
+
+        foodlist.add(
+                new FoodList(
+                        "300",
+                        R.drawable.pizza,
+                        "Burger"
+
+                ));
+
+        FoodAdapter adapter = new FoodAdapter(getContext(), foodlist);
+
+        //setting adapter to recyclerview
+        recyclerView.setAdapter(adapter);
+
+        return view;
     }
 }
